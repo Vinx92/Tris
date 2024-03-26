439a4bf0398ecd16dc04fa416200eb7308644025
Vinx92 <vv92@tiscali.it>
2024-03-26 17:22:18 +0100

    tante modifiche

--- script.js
+++ script.js
@@ -1,19 +1,71 @@
 const CASELLE = document.querySelectorAll(".casella");
 const CERCHIO = document.querySelector(".cerchio");
 const CROCE = document.querySelector(".croce");
-console.log(CASELLE);
-console.log(CERCHIO);
-console.log(CROCE);
+const GIOCATORE_UNO = {
+  nome: "ciao",
+  team: undefined,
+  turno: true,
+};
+const GIOCATORE_DUE = {
+  nome: "yoo",
+  team: undefined,
+  turno: false,
+};
+
+CERCHIO.addEventListener("click", () => {
+  if (GIOCATORE_UNO.team === undefined) {
+    GIOCATORE_UNO.team = "cerchio";
+    GIOCATORE_UNO.imgTeam = document.getElementById("scelta-uno").src;
+    document.getElementById("titolo").textContent = "SCEGLIE IL GIOCATORE DUE";
+    document.getElementById("team-giocatore-uno").src = GIOCATORE_UNO.imgTeam;
+  } else if (GIOCATORE_DUE.team === undefined) {
+    if (GIOCATORE_UNO.team === "croce") {
+      GIOCATORE_DUE.team = "cerchio";
+      GIOCATORE_DUE.imgTeam = document.getElementById("scelta-uno").src;
+      document.getElementById("team-giocatore-due").src = GIOCATORE_DUE.imgTeam;
+      document.querySelector(".scelta-team").classList.add("hide");
+      document.querySelector(".conteiner-gioco").classList.remove("hide");
+    }
+  }
+});
+
+CROCE.addEventListener("click", () => {
+  if (GIOCATORE_UNO.team === undefined) {
+    GIOCATORE_UNO.team = "croce";
+    GIOCATORE_UNO.imgTeam = document.getElementById("scelta-due").src;
+    document.getElementById("titolo").textContent = "SCEGLIE IL GIOCATORE DUE";
+    document.getElementById("team-giocatore-uno").src = GIOCATORE_UNO.imgTeam;
+  } else if (GIOCATORE_DUE.team === undefined) {
+    if (GIOCATORE_UNO.team === "cerchio") {
+      GIOCATORE_DUE.team = "croce";
+      GIOCATORE_DUE.imgTeam = document.getElementById("scelta-due").src;
+      document.getElementById("team-giocatore-due").src = GIOCATORE_DUE.imgTeam;
+      document.querySelector(".scelta-team").classList.add("hide");
+      document.querySelector(".conteiner-gioco").classList.remove("hide");
+    }
+  }
+});
 
 for (let i = 0; i < CASELLE.length; i++) {
   CASELLE[i].addEventListener("click", () => {
     if (CASELLE[i].childNodes.length === 0) {
-      const IMG = document.createElement("img");
-      IMG.src = "asset/cerchio.png";
-      IMG.alt = "icona cerchio";
-      IMG.draggable = false;
-      CASELLE[i].appendChild(IMG);
-      console.log(CASELLE[i]);
+      if (GIOCATORE_UNO.turno === true) {
+        const IMG = document.createElement("img");
+        IMG.src = GIOCATORE_UNO.imgTeam;
+        IMG.alt = "icona cerchio";
+        IMG.draggable = false;
+        CASELLE[i].appendChild(IMG);
+        GIOCATORE_UNO.turno = false;
+        GIOCATORE_DUE.turno = true;
+      } else {
+        const IMG = document.createElement("img");
+        IMG.src = GIOCATORE_DUE.imgTeam;
+        IMG.alt = "icona cerchio";
+        IMG.draggable = false;
+        CASELLE[i].appendChild(IMG);
+        GIOCATORE_UNO.turno = true;
+        GIOCATORE_DUE.turno = false;
+      }
     }
   });
 }
