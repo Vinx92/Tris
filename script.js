c6b33fc445c51560030c40a8d6ae70c0e35b0c8b
Vinx92 <vv92@tiscali.it>
2024-03-27 19:42:00 +0100

    funzionalità basi finite

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const CASELLE = document.querySelectorAll(".casella");
 const CERCHIO = document.querySelector(".cerchio");
 const CROCE = document.querySelector(".croce");
+const MESSAGGIO = document.querySelector(".messaggio-risultato");
+const PUNTEGGIO_UNO = document.getElementById("punteggio-uno");
+const PUNTEGGIO_DUE = document.getElementById("punteggio-due");
 const COMBINAZIONI = [
   [0, 1, 2],
   [3, 4, 5],
@@ -12,8 +15,10 @@ const COMBINAZIONI = [
   [2, 4, 6],
 ];
 
-let CASELLE_GIOCATORE_UNO = [];
-let CASELLE_GIOCATORE_DUE = [];
+let caselleGiocatoreUno = [];
+let caselleGiocatoreDue = [];
+
+let giocoFinito = false;
 
 const GIOCATORE_UNO = {
   team: undefined,
@@ -60,37 +65,80 @@ CROCE.addEventListener("click", () => {
 
 for (let i = 0; i < CASELLE.length; i++) {
   CASELLE[i].addEventListener("click", () => {
-    if (CASELLE[i].childNodes.length === 0) {
-      if (GIOCATORE_UNO.turno === true) {
-        const IMG = document.createElement("img");
-        IMG.src = GIOCATORE_UNO.imgTeam;
-        IMG.alt = "icona cerchio";
-        IMG.draggable = false;
-        CASELLE[i].appendChild(IMG);
-        CASELLE_GIOCATORE_UNO.push(i);
-        GIOCATORE_UNO.turno = false;
-        GIOCATORE_DUE.turno = true;
-      } else {
-        const IMG = document.createElement("img");
-        IMG.src = GIOCATORE_DUE.imgTeam;
-        IMG.alt = "icona cerchio";
-        IMG.draggable = false;
-        CASELLE[i].appendChild(IMG);
-        CASELLE_GIOCATORE_DUE.push(i);
-        GIOCATORE_UNO.turno = true;
-        GIOCATORE_DUE.turno = false;
+    if (giocoFinito === false) {
+      if (CASELLE[i].childNodes.length === 0) {
+        if (GIOCATORE_UNO.turno === true) {
+          const IMG = document.createElement("img");
+          IMG.src = GIOCATORE_UNO.imgTeam;
+          IMG.alt = "icona cerchio";
+          IMG.draggable = false;
+          CASELLE[i].appendChild(IMG);
+          caselleGiocatoreUno.push(i);
+          GIOCATORE_UNO.turno = false;
+          GIOCATORE_DUE.turno = true;
+        } else {
+          const IMG = document.createElement("img");
+          IMG.src = GIOCATORE_DUE.imgTeam;
+          IMG.alt = "icona cerchio";
+          IMG.draggable = false;
+          CASELLE[i].appendChild(IMG);
+          caselleGiocatoreDue.push(i);
+          GIOCATORE_UNO.turno = true;
+          GIOCATORE_DUE.turno = false;
+        }
+      }
+
+      for (let i = 0; i < COMBINAZIONI.length; i++) {
+        if (
+          caselleGiocatoreUno.includes(COMBINAZIONI[i][0]) &&
+          caselleGiocatoreUno.includes(COMBINAZIONI[i][1]) &&
+          caselleGiocatoreUno.includes(COMBINAZIONI[i][2])
+        ) {
+          MESSAGGIO.classList.remove("hide");
+          MESSAGGIO.textContent = "VINCE IL GIOCATORE UNO";
+          PUNTEGGIO_UNO.textContent = convertiStringa(
+            PUNTEGGIO_UNO.textContent
+          );
+          giocoFinito = true;
+        } else if (
+          caselleGiocatoreDue.includes(COMBINAZIONI[i][0]) &&
+          caselleGiocatoreDue.includes(COMBINAZIONI[i][1]) &&
+          caselleGiocatoreDue.includes(COMBINAZIONI[i][2])
+        ) {
+          MESSAGGIO.classList.remove("hide");
+          MESSAGGIO.textContent = "VINCE IL GIOCATORE DUE";
+          PUNTEGGIO_DUE.textContent = convertiStringa(
+            PUNTEGGIO_DUE.textContent
+          );
+          giocoFinito = true;
+        } else {
+          let caselleOccupate =
+            caselleGiocatoreUno.length + caselleGiocatoreDue.length;
+          if (caselleOccupate === 9) {
+            MESSAGGIO.classList.remove("hide");
+            MESSAGGIO.textContent = "PAREGGIO";
+            giocoFinito = true;
+          }
+        }
       }
     }
   });
 }
 
-
 document.getElementById("rigioca").addEventListener("click", () => {
   GIOCATORE_UNO.turno = true;
   GIOCATORE_DUE.turno = false;
-  CASELLE_GIOCATORE_UNO = [];
-  CASELLE_GIOCATORE_DUE = [];
+  caselleGiocatoreUno = [];
+  caselleGiocatoreDue = [];
+  MESSAGGIO.textContent = "";
+  giocoFinito = false;
   for (let i = 0; i < CASELLE.length; i++) {
     CASELLE[i].innerHTML = "";
   }
 });
+
+function convertiStringa(x) {
+  let numero = parseInt(x);
+  numero += 1;
+  return numero;
+}
