6d70a470d48db55ea39a6264fc347b8c6ceceafd
Vinx92 <vv92@tiscali.it>
2024-03-28 15:55:41 +0100

    aggiunto modalità di gioco, il primo ceh arriva a tre e all'infinito

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const CROCE = document.querySelector(".croce");
 const MESSAGGIO = document.querySelector(".messaggio-risultato");
 const PUNTEGGIO_UNO = document.getElementById("punteggio-uno");
 const PUNTEGGIO_DUE = document.getElementById("punteggio-due");
+const MODALITA_GIOCO = document.getElementById("modalita-gioco");
+
 const COMBINAZIONI = [
   [0, 1, 2],
   [3, 4, 5],
@@ -19,6 +21,7 @@ let caselleGiocatoreUno = [];
 let caselleGiocatoreDue = [];
 
 let giocoFinito = false;
+let modalitaGioco;
 
 const GIOCATORE_UNO = {
   team: undefined,
@@ -33,91 +36,165 @@ CERCHIO.addEventListener("click", () => {
   if (GIOCATORE_UNO.team === undefined) {
     GIOCATORE_UNO.team = "cerchio";
     GIOCATORE_UNO.imgTeam = document.getElementById("scelta-uno").src;
-    document.getElementById("titolo").textContent = "SCEGLIE IL GIOCATORE DUE";
+    document.getElementById("titolo").textContent = "GIOCATORE DUE SCEGLI IL TUO TEAM";
     document.getElementById("team-giocatore-uno").src = GIOCATORE_UNO.imgTeam;
   } else if (GIOCATORE_DUE.team === undefined) {
     if (GIOCATORE_UNO.team === "croce") {
       GIOCATORE_DUE.team = "cerchio";
       GIOCATORE_DUE.imgTeam = document.getElementById("scelta-uno").src;
       document.getElementById("team-giocatore-due").src = GIOCATORE_DUE.imgTeam;
-      document.querySelector(".scelta-team").classList.add("hide");
-      document.querySelector(".conteiner-gioco").classList.remove("hide");
+      MODALITA_GIOCO.removeAttribute('disabled')
+      document.getElementById("titolo").textContent = "ORA SCEGLIETE LA MODALITà DI GIOCO";
+      // document.querySelector(".scelta-team").classList.add("hide");
+      // document.querySelector(".conteiner-gioco").classList.remove("hide");
     }
   }
-});
+  });
 
 CROCE.addEventListener("click", () => {
   if (GIOCATORE_UNO.team === undefined) {
     GIOCATORE_UNO.team = "croce";
     GIOCATORE_UNO.imgTeam = document.getElementById("scelta-due").src;
-    document.getElementById("titolo").textContent = "SCEGLIE IL GIOCATORE DUE";
+    document.getElementById("titolo").textContent = "GIOCATORE DUE SCEGLI IL TUO TEAM";
     document.getElementById("team-giocatore-uno").src = GIOCATORE_UNO.imgTeam;
   } else if (GIOCATORE_DUE.team === undefined) {
     if (GIOCATORE_UNO.team === "cerchio") {
       GIOCATORE_DUE.team = "croce";
       GIOCATORE_DUE.imgTeam = document.getElementById("scelta-due").src;
       document.getElementById("team-giocatore-due").src = GIOCATORE_DUE.imgTeam;
-      document.querySelector(".scelta-team").classList.add("hide");
-      document.querySelector(".conteiner-gioco").classList.remove("hide");
+      MODALITA_GIOCO.removeAttribute('disabled')
+      document.getElementById("titolo").textContent = "ORA SCEGLIETE LA MODALITà DI GIOCO";
+      // document.querySelector(".scelta-team").classList.add("hide");
+      // document.querySelector(".conteiner-gioco").classList.remove("hide");
     }
   }
-});
+  });
+
+MODALITA_GIOCO.addEventListener('change', () => {
+    modalitaGioco = MODALITA_GIOCO.value
+document.querySelector(".scelta-team").classList.add("hide");
+      document.querySelector(".conteiner-gioco").classList.remove("hide");
+
+})
 
 for (let i = 0; i < CASELLE.length; i++) {
   CASELLE[i].addEventListener("click", () => {
-    if (giocoFinito === false) {
-      if (CASELLE[i].childNodes.length === 0) {
-        if (GIOCATORE_UNO.turno === true) {
-          const IMG = document.createElement("img");
-          IMG.src = GIOCATORE_UNO.imgTeam;
-          IMG.alt = "icona cerchio";
-          IMG.draggable = false;
-          CASELLE[i].appendChild(IMG);
-          caselleGiocatoreUno.push(i);
-          GIOCATORE_UNO.turno = false;
-          GIOCATORE_DUE.turno = true;
-        } else {
-          const IMG = document.createElement("img");
-          IMG.src = GIOCATORE_DUE.imgTeam;
-          IMG.alt = "icona cerchio";
-          IMG.draggable = false;
-          CASELLE[i].appendChild(IMG);
-          caselleGiocatoreDue.push(i);
-          GIOCATORE_UNO.turno = true;
-          GIOCATORE_DUE.turno = false;
-        }
-      }
+    if(modalitaGioco === 'megliotre') {
+      if(PUNTEGGIO_UNO.textContent < 3 && PUNTEGGIO_DUE.textContent < 3){
 
-      for (let i = 0; i < COMBINAZIONI.length; i++) {
-        if (
-          caselleGiocatoreUno.includes(COMBINAZIONI[i][0]) &&
-          caselleGiocatoreUno.includes(COMBINAZIONI[i][1]) &&
-          caselleGiocatoreUno.includes(COMBINAZIONI[i][2])
-        ) {
-          MESSAGGIO.classList.remove("hide");
-          MESSAGGIO.textContent = "VINCE IL GIOCATORE UNO";
-          PUNTEGGIO_UNO.textContent = convertiStringa(
-            PUNTEGGIO_UNO.textContent
-          );
-          giocoFinito = true;
-        } else if (
-          caselleGiocatoreDue.includes(COMBINAZIONI[i][0]) &&
-          caselleGiocatoreDue.includes(COMBINAZIONI[i][1]) &&
-          caselleGiocatoreDue.includes(COMBINAZIONI[i][2])
-        ) {
-          MESSAGGIO.classList.remove("hide");
-          MESSAGGIO.textContent = "VINCE IL GIOCATORE DUE";
-          PUNTEGGIO_DUE.textContent = convertiStringa(
-            PUNTEGGIO_DUE.textContent
-          );
-          giocoFinito = true;
-        } else {
-          let caselleOccupate =
-            caselleGiocatoreUno.length + caselleGiocatoreDue.length;
-          if (caselleOccupate === 9) {
+        if (giocoFinito === false) {
+          if (CASELLE[i].childNodes.length === 0) {
+            if (GIOCATORE_UNO.turno === true) {
+              const IMG = document.createElement("img");
+              IMG.src = GIOCATORE_UNO.imgTeam;
+              IMG.alt = "icona cerchio";
+              IMG.draggable = false;
+              CASELLE[i].appendChild(IMG);
+              caselleGiocatoreUno.push(i);
+              GIOCATORE_UNO.turno = false;
+              GIOCATORE_DUE.turno = true;
+            } else {
+              const IMG = document.createElement("img");
+              IMG.src = GIOCATORE_DUE.imgTeam;
+              IMG.alt = "icona cerchio";
+              IMG.draggable = false;
+              CASELLE[i].appendChild(IMG);
+              caselleGiocatoreDue.push(i);
+              GIOCATORE_UNO.turno = true;
+              GIOCATORE_DUE.turno = false;
+            }
+          }
+    
+          for (let i = 0; i < COMBINAZIONI.length; i++) {
+            if (
+              caselleGiocatoreUno.includes(COMBINAZIONI[i][0]) &&
+              caselleGiocatoreUno.includes(COMBINAZIONI[i][1]) &&
+              caselleGiocatoreUno.includes(COMBINAZIONI[i][2])
+            ) {
+              MESSAGGIO.classList.remove("hide");
+              MESSAGGIO.textContent = "VINCE IL GIOCATORE UNO";
+              PUNTEGGIO_UNO.textContent = convertiStringa(
+                PUNTEGGIO_UNO.textContent
+              );
+              giocoFinito = true;
+            } else if (
+              caselleGiocatoreDue.includes(COMBINAZIONI[i][0]) &&
+              caselleGiocatoreDue.includes(COMBINAZIONI[i][1]) &&
+              caselleGiocatoreDue.includes(COMBINAZIONI[i][2])
+            ) {
+              MESSAGGIO.classList.remove("hide");
+              MESSAGGIO.textContent = "VINCE IL GIOCATORE DUE";
+              PUNTEGGIO_DUE.textContent = convertiStringa(
+                PUNTEGGIO_DUE.textContent
+              );
+              giocoFinito = true;
+            } else {
+              let caselleOccupate =
+                caselleGiocatoreUno.length + caselleGiocatoreDue.length;
+              if (caselleOccupate === 9) {
+                MESSAGGIO.classList.remove("hide");
+                MESSAGGIO.textContent = "PAREGGIO";
+                giocoFinito = true;
+              }
+            }
+          }
+        }
+      } 
+    } else {
+      if (giocoFinito === false) {
+        if (CASELLE[i].childNodes.length === 0) {
+          if (GIOCATORE_UNO.turno === true) {
+            const IMG = document.createElement("img");
+            IMG.src = GIOCATORE_UNO.imgTeam;
+            IMG.alt = "icona cerchio";
+            IMG.draggable = false;
+            CASELLE[i].appendChild(IMG);
+            caselleGiocatoreUno.push(i);
+            GIOCATORE_UNO.turno = false;
+            GIOCATORE_DUE.turno = true;
+          } else {
+            const IMG = document.createElement("img");
+            IMG.src = GIOCATORE_DUE.imgTeam;
+            IMG.alt = "icona cerchio";
+            IMG.draggable = false;
+            CASELLE[i].appendChild(IMG);
+            caselleGiocatoreDue.push(i);
+            GIOCATORE_UNO.turno = true;
+            GIOCATORE_DUE.turno = false;
+          }
+        }
+  
+        for (let i = 0; i < COMBINAZIONI.length; i++) {
+          if (
+            caselleGiocatoreUno.includes(COMBINAZIONI[i][0]) &&
+            caselleGiocatoreUno.includes(COMBINAZIONI[i][1]) &&
+            caselleGiocatoreUno.includes(COMBINAZIONI[i][2])
+          ) {
             MESSAGGIO.classList.remove("hide");
-            MESSAGGIO.textContent = "PAREGGIO";
+            MESSAGGIO.textContent = "VINCE IL GIOCATORE UNO";
+            PUNTEGGIO_UNO.textContent = convertiStringa(
+              PUNTEGGIO_UNO.textContent
+            );
             giocoFinito = true;
+          } else if (
+            caselleGiocatoreDue.includes(COMBINAZIONI[i][0]) &&
+            caselleGiocatoreDue.includes(COMBINAZIONI[i][1]) &&
+            caselleGiocatoreDue.includes(COMBINAZIONI[i][2])
+          ) {
+            MESSAGGIO.classList.remove("hide");
+            MESSAGGIO.textContent = "VINCE IL GIOCATORE DUE";
+            PUNTEGGIO_DUE.textContent = convertiStringa(
+              PUNTEGGIO_DUE.textContent
+            );
+            giocoFinito = true;
+          } else {
+            let caselleOccupate =
+              caselleGiocatoreUno.length + caselleGiocatoreDue.length;
+            if (caselleOccupate === 9) {
+              MESSAGGIO.classList.remove("hide");
+              MESSAGGIO.textContent = "PAREGGIO";
+              giocoFinito = true;
+            }
           }
         }
       }
@@ -126,7 +203,21 @@ for (let i = 0; i < CASELLE.length; i++) {
 }
 
 document.getElementById("rigioca").addEventListener("click", () => {
-  GIOCATORE_UNO.turno = true;
+  if(modalitaGioco == 'megliotre'){
+    if(PUNTEGGIO_UNO.textContent == 3 || PUNTEGGIO_DUE.textContent == 3) {
+      GIOCATORE_UNO.turno = true;
+  GIOCATORE_DUE.turno = false;
+  caselleGiocatoreUno = [];
+  caselleGiocatoreDue = [];
+  MESSAGGIO.textContent = "";
+  PUNTEGGIO_UNO.textContent = 0
+  PUNTEGGIO_DUE.textContent = 0
+  giocoFinito = false;
+  for (let i = 0; i < CASELLE.length; i++) {
+    CASELLE[i].innerHTML = "";
+  }
+    } else {
+      GIOCATORE_UNO.turno = true;
   GIOCATORE_DUE.turno = false;
   caselleGiocatoreUno = [];
   caselleGiocatoreDue = [];
@@ -134,7 +225,20 @@ document.getElementById("rigioca").addEventListener("click", () => {
   giocoFinito = false;
   for (let i = 0; i < CASELLE.length; i++) {
     CASELLE[i].innerHTML = "";
-  }
+  }  
+    }
+    
+  } else {
+    GIOCATORE_UNO.turno = true;
+  GIOCATORE_DUE.turno = false;
+  caselleGiocatoreUno = [];
+  caselleGiocatoreDue = [];
+  MESSAGGIO.textContent = "";
+  giocoFinito = false;
+  for (let i = 0; i < CASELLE.length; i++) {
+    CASELLE[i].innerHTML = "";
+  }}
+  
 });
 
 function convertiStringa(x) {
